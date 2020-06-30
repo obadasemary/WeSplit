74bd18c1d2860f0dde3b77fac80b5454df2fcccb
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-06-30 22:03:14 +0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Abdelrahman Mohamed on 30.06.2020.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
