9097138ce73f875444188347c28054f4859f7c63
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-07-03 23:27:22 +0300

    Finalize WeSplit (#1)
    * feat: Use Forms
    
    * Add NavigationView
    
    * Modifying program state
    
    * feat: Binding state to user interface controls
    
    BREAKING CHANGE: Binding state to user interface controls by using textfield to enter your name and text view to show your name immediately
    
    Obada 🚀😎
    
    * feat: Have Picker View inside form
    
    BREAKING CHANGE:
    
    * Reading text from the user with TextField
    
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>
    
    * Creating pickers in a form
    
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>
    
    * Adding a segmented control for tip percentages
    
    Adding a segmented control for tip percentages – WeSplit SwiftUI Tutorial 9/10
    
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>
    
    * Calculating the total per person
    
    Calculating the total per person – WeSplit SwiftUI Tutorial 10/10
    
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,8 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+
+//    let gender = ["Male", "Female"]
+//    @State private var selectedGender = "Male"
+//    @State private var tapCount = 0
+//    @State private var name = ""
+
+    @State private var checkAmount = ""
+    @State private var numberOfPepole = 2
+    @State private var tipPercentage = 2
+
+    let tipPercentages = [10, 15, 20, 25, 0]
+
+    var totalPerPerson: Double {
+
+        let pepoleCount = Double(numberOfPepole + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / pepoleCount
+
+        return amountPerPerson
+    }
+
     var body: some View {
-        Text("Hello, world!").padding()
+
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amont", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    Picker("Number of pepole", selection: $numberOfPepole) {
+                        ForEach(2 ..< 101) {
+                            Text("\($0) pepole")
+                        }
+                    }
+                }
+
+                Section(header: Text("How much tip do you want to leave?")) {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+
+                Section {
+                    Text("₺ \(totalPerPerson, specifier: "%.2f")")
+                }
+            }
+            .navigationBarTitle("WeSplit")
+        }
     }
 }
 
